8d3d5f4f7f9c922fc5a34ad08c7762e00f0f53a7
jigsaw <m@jgs.me>
2023-05-24 09:14:13 +0000

    add links

--- src/app/Top.tsx
+++ src/app/Top.tsx
@@ -15,7 +15,15 @@ const Inner = () => {
   const { status } = useSession();
   if (status === "authenticated") {
     return (
-      <div>
+      <div className="pt-2">
+        <div className="flex justify-center text-xl gap-6">
+          <Link href="/images" className="border border-solid p-1">
+            images
+          </Link>
+          <Link href="/images?rate=true" className="border border-solid p-1">
+            images by rate
+          </Link>
+        </div>
         <Uploader></Uploader>
         <div>
           <Link href="/settings">settings</Link>
